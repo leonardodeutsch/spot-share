1eb6cbfbaeef70767af9b6f145aeb742b3c4d631
Leo <deutschleonardo@gmail.com>
2022-02-12 21:54:55 -0600

    implemented address geocoding and google map

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-// Code goes here!
+import API_KEY from '../config';
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  
+  axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`)
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch Location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 8,
+      });
+
+      new google.maps.Marker({position: coordinates, map:map});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+form.addEventListener('submit', searchAddressHandler);
